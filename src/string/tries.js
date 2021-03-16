3662abfd2dfd42c5bc4594a7d338a2037f97cb94
liuxiao <liuxiao@oro.com>
2021-03-16 09:11:39 +0800

    search and deletion done. init TernaryST

--- src/string/tries.js
+++ src/string/tries.js
@@ -1,3 +1,9 @@
+/*
+TODO:
+ 1. Why d === key.length used in StringST rather than  d === length - 1 used in TernaryST.
+
+*/
+
 import { Queue } from './utils'
 
 function Node(value = null, R = 256) {
@@ -42,12 +48,13 @@ export function StringST(a, options = {}) {
 StringST.prototype = {
   constructor: StringST,
   get(key) {
-    return this.getNode(this.root, key, 0).value
+    const node = this.getNode(this.root, key, 0)
+    return node && node.value
   },
   getNode(node, key, d) {
-    console.log(`getNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
+    // console.log(`getNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
     if(!node)
-      return
+      return null
     if(d === key.length)
       return node
     return this.getNode(node.next[charCodeAtWithOffset(key, d, this.offset)], key, d + 1)
@@ -84,7 +91,7 @@ StringST.prototype = {
     return q
   },
   collect(node, s, q) {
-    console.log(`collect -- node: ${node && node.toString()}, key: ${s}, q: ${q.toString()}`)
+    // console.log(`collect -- node: ${node && node.toString()}, key: ${s}, q: ${q.toString()}`)
     if(!node)
       return
 
@@ -98,22 +105,22 @@ StringST.prototype = {
     return this.search(this.root, s, 0, 0)
   },
   search(node, s, d, len) {
-    if(node != null) {
+    if(node == null || d > s.length)
+      return len
 
-    }
+    const c = charCodeAtWithOffset(s, d, this.offset)
 
-    if(d > s.length)
-
-    if(node.value === charCodeAtWithOffset(s, d, this.offset))
-    this.search()
+    if(node.value != null)
+      return this.search(node.next[c], s, d + 1, d)
+    return this.search(node.next[c], s, d + 1, len)
   },
   delete(key) {
     return this.deleteNode(this.root, key, 0)
   },
   deleteNode(node, key, d) {
-    console.log(`deleteNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
+    // console.log(`deleteNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
     if(node == null)
-      return
+      return null
 
     if(d === key.length) {
       node.value = null
@@ -133,85 +140,87 @@ StringST.prototype = {
   },
 }
 
-/*
-To find the longest key that is a prefix of a given string, we use a recursive
-method like get() that keeps track of the length of the longest key found on the
-search path (by passing it as a parameter to the recursive method, updating the value
-whenever a node with a non-null value is encountered). The
-search ends when the end of the string or a null link is encountered,
-whichever comes first.
-
-Deletion. The first step needed to delete a key-value pair
-from a trie is to use a normal search to find the node corresponding
-to the key and set the corresponding value to
-null. If that node has a non-null link to a child, then no
-more work is required; if all the links are null, we need to
-remove the node from the data structure. If doing so leaves
-all the links null in its parent, we need to remove that node,
-and so forth. The implementation on the facing page demonstrates
-that this action can be accomplished with remarkably
-little code, using our standard recursive setup: after the
-recursive calls for a node x, we return null if the client value
-and all of the links in a node are null; otherwise we return
-x.
-
-private Node delete(Node x, String key, int d)
-{
-  if (x == null) return null;
-  if (d == key.length())
-  x.val = null;
-  else
-  {
-  char c = key.charAt(d);
-  x.next[c] = delete(x.next[c], key, d+1);
-  }
-  if (x.val != null) return x;
-  for (char c = 0; c < R; c++)
-  if (x.next[c] != null) return x;
-  return null;
+function TernaryNode(c) {
+  this.c = c
+  this.left = null
+  this.mid = null
+  this.right = null
+  this.value = null
 }
 
-public Iterable<String> keysWithPrefix(String pre)
-{
-  Queue<String> q = new Queue<String>();
-  collect(get(root, pre, 0), pre, q);
-  return q;
-}
-private void collect(Node x, String pre, Queue<String> q)
-{
-  if (x == null) return;
-  if (x.val != null) q.enqueue(pre);
-
-  for (char c = 0; c < R; c++)
-    collect(x.next[c], pre + c, q);
+TernaryNode.prototype.toString = function() {
+  return `Node ${this.c}, Value ${this.value}`
 }
 
-Because characters and keys are represented implicitly in tries, providing
-clients with the ability to iterate through the keys presents a challenge.
+export function TernaryST(a, options = {}) {
+  this.root = new TernaryNode(-1)
+  this.size = 0
+  this.R = options.R || 256
+  this.offset = options.offset || 0
 
-As with binary
-search trees, we accumulate the string keys in a Queue, but for tries we need to create
-explicit representations of all of the string keys, not just find them in the data structure.
-We do so with a recursive private method collect() that is similar to size() but also
-maintains a string with the sequence of characters on the path from the root.
+  if(typeof a === 'string')
+    this.put(a, this.size)
+  else if(Array.isArray(a))
+    a.forEach((one, i) => {
+      if(typeof one === 'string')
+        this.put(one, i)
+      else if(typeof one === 'object') {
+        const { key, value } = one
+        this.put(key, value)
+      } else
+        throw new Error(`cannot parse ${a} with item: ${one}`)
+    })
+  else if(typeof a === 'object')
+    this.put(a.key, a.value)
+}
 
-Each time
-that we visit a node via a call to collect() with that node as first argument, the second
-argument is the string associated with that node (the sequence of
-characters on the path from the
-root to the node). To visit a node,
-we add its associated string to the
-queue if its value is not null, then
-visit (recursively) all the nodes
-in its array of links, one for each
-possible character. To create the
-key for each call, we append the
-character corresponding to the
-link to the current key. We use
-this collect() method to collect
-keys for both the keys() and
-the keysWithPrefix() methods
-in the API. To implement keys()
-we call keysWithPrefix() with
-the empty string as argument;
-*/
+TernaryST.prototype = {
+  constructor: TernaryST,
+  getSize() {
+    return this.size
+  },
+  get(key) {
+    const node = this.getNode(this.root, key, 0)
+    return node && node.value
+  },
+  getNode(node, key, d) {
+    // console.log(`getNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
+    if(node == null)
+      return null
+
+    const c = charCodeAtWithOffset(key, d, this.offset)
+
+    if(c < node.c)
+      return this.getNode(node.left, key, d)
+    if(c > node.c)
+      return this.getNode(node.right, key, d)
+    if(d < key.length - 1)
+      return this.getNode(node.mid, key, d + 1)
+
+    return node
+  },
+  put(key, value) {
+    return this.putNode(this.root, key, value, 0)
+  },
+  putNode(node, key, value, d) {
+    // console.log(`putNode -- node: ${node && node.toString()}, key: ${key}, d: ${d}`)
+    const c = charCodeAtWithOffset(key, d, this.offset)
+
+    if(node == null)
+      node = new TernaryNode(c)
+
+    if(c < node.c)
+      node.left = this.putNode(node.left, key, value, d)
+    else if(c > node.c)
+      node.right = this.putNode(node.right, key, value, d)
+    else if(d < key.length - 1)
+      node.mid = this.putNode(node.mid, key, value, d + 1)
+    else {
+      if(node.value == null)
+        this.size += 1
+      node.value = value
+    }
+
+    return node
+  },
+}
--- test/string/tries.test.js
+++ test/string/tries.test.js
@@ -1,34 +1,104 @@
+const path = require('path')
+const { StringST, TernaryST } = require(path.join(process.cwd(), 'src/string/tries'))
+
+const TEST = `she sells sea shells by the sea shore`.split(/\s+/)
+const smallAlphabetOptions = { R: 26, offset: 97 }
+
 describe('tries test cases', () => {
-  const path = require('path')
-  const { StringST } = require(path.join(process.cwd(), 'src/string/tries'))
+  let st = null
+  let set = null
+  let noDuplicatedTest = null
 
-  const TEST = `she sells sea shells by the sea shore`.split(/\s+/)
+  beforeEach(() => {
+    st = new StringST(TEST, smallAlphabetOptions)
+    set = new Set()
 
-  const smallAlphabetOptions = { R: 26, offset: 97 }
-  const st = new StringST(TEST, smallAlphabetOptions)
-  const set = new Set()
+    for(let s of TEST)
+      set.add(s)
 
-  for(let s of TEST)
-    set.add(s)
-
-  const noDuplicatedTest = [ ...set ]
+    noDuplicatedTest = [ ...set ]
+  })
 
   it('can get key', () => {
-    // expect(st.get('she')).toBe(TEST.lastIndexOf('she'))
-    // expect(st.get('shore')).toBe(TEST.lastIndexOf('shore'))
-    // expect(st.get('shells')).toBe(TEST.lastIndexOf('shells'))
+    expect(st.get('she')).toBe(TEST.lastIndexOf('she'))
+    expect(st.get('shore')).toBe(TEST.lastIndexOf('shore'))
+    expect(st.get('shells')).toBe(TEST.lastIndexOf('shells'))
+    expect(st.get('shel')).toBeNull()
+    expect(st.get('zz')).toBeNull()
+    expect(st.get('sh')).toBeNull()
     expect(st.getSize()).toBe(noDuplicatedTest.length)
   })
 
-  // it('shows keysWithPrefix', () => {
-  //   console.log(st.keysWithPrefix(`she`))
-  // })
+  it('shows keysWithPrefix', () => {
+    expect(
+      st.keysWithPrefix(`she`).toString().trim()
+    ).toEqual('she, shells')
+  })
 
   it('can delete a key', () => {
-    // st.put('shels')
+    const size = st.getSize()
     st.delete('shells')
+    expect(st.getSize()).toBe(size - 1)
+    expect(st.get('shells')).toBeNull()
+  })
+
+  it('search a given key', () => {
+    expect(st.longestPrefixOf('she')).toBe(3)
+    expect(st.longestPrefixOf('shell')).toBe(3)
+    expect(st.longestPrefixOf('shells')).toBe(6)
   })
 })
+
+describe('TernaryST test cases', () => {
+  let st = null
+  let set = null
+  let noDuplicatedTest = null
+
+  beforeEach(() => {
+    st = new TernaryST(TEST, smallAlphabetOptions)
+    set = new Set()
+
+    for(let s of TEST)
+      set.add(s)
+
+    noDuplicatedTest = [ ...set ]
+  })
+
+  it('init TernaryST', () => {
+    expect(st.get('she')).toBe(TEST.lastIndexOf('she'))
+    expect(st.get('shore')).toBe(TEST.lastIndexOf('shore'))
+    expect(st.get('shells')).toBe(TEST.lastIndexOf('shells'))
+    expect(st.get('shel')).toBeNull()
+    expect(st.get('zz')).toBeNull()
+    expect(st.get('sh')).toBeNull()
+
+    expect(st.getSize()).toBe(noDuplicatedTest.length)
+  })
+})
+
+
+describe('EXERCISES', () => {
+  /*
+  5.2.1
+    Draw the R-way trie that results when the keys
+      no is th ti fo al go pe to co to th ai of th pa
+    are inserted in that order into an initially empty trie (do not draw null links).
+  5.2.2
+    Draw the TST that results when the keys
+      no is th ti fo al go pe to co to th ai of th pa
+    are inserted in that order into an initially empty TST.
+  5.2.3
+    Draw the R-way trie that results when the keys
+      now is the time for all good people to come to the aid of
+    are inserted in that order into an initially empty trie (do not draw null links).
+
+  */
+  it('5.2.1', () => {
+    const TEST521 = `no is th ti fo al go pe to co to th ai of th pa`
+
+  })
+})
+
 
 /*
 s
